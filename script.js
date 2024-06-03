e21f9f8c92e7a37e81cff61f648c331f160ef751
tommaho <tom@tommaho.com>
2024-06-03 14:59:40 -0400

    Change grid square colors by clicking.

--- script.js
+++ script.js
@@ -8,15 +8,27 @@ const containerWidth = container.clientWidth;
 
 const squaresPerRow = gridSquares / 4;
 
-const squareWidth = containerWidth / squaresPerRow;
+const squareWidth = containerWidth / squaresPerRow - 2; //-2 for border
 
 console.log(`Container width: ${containerWidth}.`);
 console.log(`Square width: ${squareWidth}.`);
 
 for (let i = 0; i < gridSquares; i++) {
   let nextCell = document.createElement('div');
+
   nextCell.style.height = squareWidth + 'px';
   nextCell.style.width = squareWidth + 'px';
+  nextCell.className = 'grid-square';
+  nextCell.id = 'grid-square-' + i;
+
+  nextCell.addEventListener('click', (e) => {
+    changeColor(nextCell.id);
+  });
 
   container.append(nextCell);
+}
+
+function changeColor(gridSquare) {
+  let target = document.querySelector('#' + gridSquare);
+  target.style.backgroundColor = 'red';
 }
