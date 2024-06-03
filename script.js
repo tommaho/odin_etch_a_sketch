7abbb5af16e95dab48ad77e67312de332c7d574b
tommaho <tom@tommaho.com>
2024-06-03 14:16:46 -0400

    create grid squares in js, based on hard-coded container size and square count

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Etch-A-Sketch
+// Etch-A-Sketch JS
+// Author: tommaho
+
+const gridSquares = 16;
+const container = document.querySelector('#container');
+
+const containerWidth = container.clientWidth;
+
+const squaresPerRow = gridSquares / 4;
+
+const squareWidth = containerWidth / squaresPerRow;
+
+console.log(`Container width: ${containerWidth}.`);
+console.log(`Square width: ${squareWidth}.`);
+
+for (let i = 0; i < gridSquares; i++) {
+  let nextCell = document.createElement('div');
+  nextCell.style.height = squareWidth + 'px';
+  nextCell.style.width = squareWidth + 'px';
+
+  container.append(nextCell);
+}
