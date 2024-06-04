2a5e6bfbc00e8b8d89985c62dde0aad78e4bc43f
tommaho <tom@tommaho.com>
2024-06-04 09:24:07 -0400

    Reset and re-initialize 16 square grid via button click.

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
 // Etch-A-Sketch JS
 // Author: tommaho
 
-const gridSquares = 16;
-const container = document.querySelector('#container');
-const containerWidth = container.clientWidth;
-const squaresPerRow = Math.sqrt(gridSquares);
-const squareWidth = containerWidth / squaresPerRow - 2; //-2 for border
-
 const defaultColor = 'cornflowerblue';
 const selectedColor = 'grey';
 
+function initializeGrid(gridSquares = 16) {
+  //const gridSquares = 16;
+  const container = document.querySelector('#container');
+  const containerWidth = container.clientWidth;
+  const squaresPerRow = Math.sqrt(gridSquares);
+  const squareWidth = containerWidth / squaresPerRow - 2; //-2 for border
+
+  for (let i = 0; i < gridSquares; i++) {
+    let nextCell = document.createElement('div');
+
+    nextCell.style.height = squareWidth + 'px';
+    nextCell.style.width = squareWidth + 'px';
+    nextCell.className = 'grid-square';
+    nextCell.id = 'grid-square-' + i;
+
+    // function changeColor(gridSquare) {
+    //     let target = document.querySelector('#' + gridSquare);
+    //     target.style.backgroundColor = selectedColor;
+    //     target.classList.add('touched');
+    //   }
+
+    nextCell.addEventListener('mouseover', (e) => {
+      // changeColor(nextCell.id);
+      let target = document.querySelector('#' + nextCell.id);
+      target.style.backgroundColor = selectedColor;
+      target.classList.add('touched');
+    });
+
+    container.append(nextCell);
+  }
+}
+
 const resetButton = document.querySelector('#reset');
 
-resetButton.addEventListener('click', () => {
-  const gridSquares = document.querySelectorAll('.touched');
+const newLayoutButton = document.querySelector('#new');
+
+newLayoutButton.addEventListener('click', () => {
+  const gridSquares = document.querySelectorAll('.grid-square');
   gridSquares.forEach((square) => {
+    square.remove();
+  });
+
+  initializeGrid();
+});
+
+resetButton.addEventListener('click', () => {
+  const touchedGridSquares = document.querySelectorAll('.touched');
+  touchedGridSquares.forEach((square) => {
     square.style.backgroundColor = defaultColor;
+    square.classList.remove('touched');
   });
 });
 
-console.log(`Container width: ${containerWidth}.`);
-console.log(`Square width: ${squareWidth}.`);
-
-for (let i = 0; i < gridSquares; i++) {
-  let nextCell = document.createElement('div');
-
-  nextCell.style.height = squareWidth + 'px';
-  nextCell.style.width = squareWidth + 'px';
-  nextCell.className = 'grid-square';
-  nextCell.id = 'grid-square-' + i;
-
-  nextCell.addEventListener('mouseover', (e) => {
-    changeColor(nextCell.id);
-  });
-
-  container.append(nextCell);
-}
-
-function changeColor(gridSquare) {
-  let target = document.querySelector('#' + gridSquare);
-  target.style.backgroundColor = selectedColor;
-  target.classList.add('touched');
-}
+// console.log(`Container width: ${containerWidth}.`);
+// console.log(`Square width: ${squareWidth}.`);
